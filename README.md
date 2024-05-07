94a453bfe00621ff64f31eaf365a64b3cdf5773c
JENIPHER <jeniperkuki@gmail.com>
2024-05-07 21:51:29 +0300

    createPool modele in mysql libraary

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ RestApi with Node JS, Express, Swagger and MySQL
 # Note
 - execute file (node [filename]) e.g. node index.js
 - express is used to overcome the challange of terminate/exit inorder to increase efficiency
+- Dotenv package helps keep sensitive information encrypted
---
+++ config/database.js
@@ -0,0 +1,14 @@
+//importing method
+const {createPool} = require('mysql');
+
+const pool = createPool({
+    port:3306,
+    host: 'localhst',
+    user:'root',
+    password: '',
+    database:'test',
+    connectionLimit:10 //how many connections we want to create in a pool
+})
+
+// export module as pool (helps in reuse of connection)
+module.exports = pool;
